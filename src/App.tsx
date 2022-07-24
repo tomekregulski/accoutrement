7d4c45a05f8457d8e7fb4388e43c2d2a2dbdb28f
Tomek Regulski <tomek.regulski@gmail.com>
2022-07-24 10:46:27 -0400

    testing pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function App() {
-  return <h1>Hello world! I am using React</h1>;
+	return (
+		<>
+			<h1>Hello world! I am using React</h1>
+			<p>Now with pre-commit hooks via husky.</p>
+		</>
+	);
 }
