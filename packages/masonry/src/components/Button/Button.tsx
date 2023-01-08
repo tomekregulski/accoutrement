6ea84d3b5ef9c14cd828c370fa608ca97817c9b4
Tomek Regulski <tomek.regulski@gmail.com>
2023-01-08 18:22:08 -0500

    install tailwind and twin.macro

--- packages/masonry/src/components/Button/Button.tsx
+++ packages/masonry/src/components/Button/Button.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import tw from 'twin.macro';
 
 export interface ButtonProps {
   label: string;
   onClick: () => void;
+  css: any;
 }
+
+const StyledButton = tw.button`text-blue-500 text-2xl p-2`;
 
 export function Button(props: ButtonProps) {
   const { label, onClick } = props;
 
-  return <button onClick={onClick}>{label}</button>;
+  return <StyledButton onClick={onClick}>{label}</StyledButton>;
 }
