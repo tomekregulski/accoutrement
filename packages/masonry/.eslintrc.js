99f4ad5f672f60450cebf8b8f7ceb2cdd8c5a0e5
Tomek Regulski <tomek.regulski@gmail.com>
2023-01-08 19:30:07 -0500

    adjust prettier settings

--- packages/masonry/.eslintrc.js
+++ packages/masonry/.eslintrc.js
@@ -2,14 +2,23 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
+    jest: true,
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
   overrides: [],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json'],
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['react'],
-  rules: {},
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
 };
